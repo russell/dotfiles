0781c9e8479f159ced7a6620a2ffc82381ddab80
Russell Sim <russell.sim@jcu.edu.au>
2008-09-22 11:34:49 +1000

    fixed copyright so that it defaults to the current year

--- vim/skeletons/python.py
+++ vim/skeletons/python.py
@@ -1,7 +1,11 @@
+<tskel:before>
+let b:tskelDateFormat = '%Y'
+</tskel:before>
 ##############################################################################
 #
-# Copyright (c) 2006 James Cook University and Contributors.
+# Copyright (c) <+DATE+> James Cook University and Contributors.
 # All Rights Reserved.
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
