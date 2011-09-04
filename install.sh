4ec0d85c6629e545a1e3b0056731a8ca4f433871
Russell Sim <russell.sim@gmail.com>
2011-09-04 18:57:20 +1000

    added more debug printing and exclude other tempory files

--- install.sh
+++ install.sh
@@ -10,10 +10,12 @@ for name in *; do
 		echo "WARNING: $target exists and is a symlink."
 	fi
     else
-	if [[ $name != 'install.sh' && $name != 'README' && $name != *~  && $name != *.orig ]]; then
+	if [[ $name != 'install.sh' && $name != 'README' && $name != *~  && $name != *.orig && $name != \#*\# ]]; then
 	    if [[ $name == *.local ]]; then
+		echo "added local file $target"
 		cp "$PWD/$name" "$target"
 	    else
+		echo "linked in $target"
 		ln -s "$PWD/$name" "$target"
 	    fi
 	fi
