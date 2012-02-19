40d96603696a78b15a818299555cf2723e7f0033
Russell Sim <russell.sim@gmail.com>
2012-02-19 19:34:23 +1100

    added size indication mode

--- emacs.d/init.d/25-gui.el
+++ emacs.d/init.d/25-gui.el
@@ -13,3 +13,6 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;;; Display file size in Modeline
+(size-indication-mode 1)
