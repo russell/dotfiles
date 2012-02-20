5120742ad87c4519cfd8dbc6ef31849327c751ae
Russell Sim <russell.sim@gmail.com>
2012-02-20 21:34:20 +1100

    added ibuffer ido find files

--- emacs.d/init-mode.d/40-ibuffer.el
+++ emacs.d/init-mode.d/40-ibuffer.el
@@ -22,3 +22,18 @@
 (add-hook 'ibuffer-mode-hook
 	  (lambda ()
 	    (ibuffer-switch-to-saved-filter-groups "default")))
+
+(defun ibuffer-ido-find-file ()
+  "Like `ido-find-file', but default to the directory of the buffer at point."
+  (interactive
+   (let ((default-directory (let ((buf (ibuffer-current-buffer)))
+			      (if (buffer-live-p buf)
+				  (with-current-buffer buf
+				    default-directory)
+				default-directory))))
+     (ido-find-file-in-dir default-directory))))
+
+(add-hook 'ibuffer-mode-hook
+          (lambda ()
+            (define-key ibuffer-mode-map "\C-x\C-f"
+              'ibuffer-ido-find-file)))
