0bffc5ed53211a1fd091faa676a707c750d5dcc2
Russell Sim <russell.sim@gmail.com>
2012-03-26 19:34:46 +1100

    added omit mode to dired

--- emacs.d/init-mode.d/40-dired.el
+++ emacs.d/init-mode.d/40-dired.el
@@ -1,2 +1,7 @@
 ;; C-x C-j opens dired with the cursor right on the file you're editing
 (require 'dired-x)
+
+(setq-default dired-omit-files-p t) ; this is buffer-local variable
+
+(setq dired-omit-files
+      (concat dired-omit-files "\\|^\\..+$"))
