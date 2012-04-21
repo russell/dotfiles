8d0a426593621ae3ed2eea4042373fab4ac425a4
Russell Sim <russell.sim@gmail.com>
2012-04-21 17:51:48 +1000

    added ediff mode configuration

---
+++ emacs.d/init-mode.d/40-ediff.el
@@ -0,0 +1,9 @@
+
+;; This is what you probably want if you are using a tiling window
+;; manager under X, such as ratpoison.
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;; Split the window depending on the current frame size.
+(setq ediff-split-window-function (if (> (frame-width) 150)
+                                      'split-window-horizontally
+                                    'split-window-vertically))
