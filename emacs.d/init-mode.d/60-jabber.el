b7a2a60751b2748aa7dd291930f4ae869ea0ecd9
Russell Sim <russell.sim@gmail.com>
2012-05-22 11:35:26 +1000

    added jabber mode

---
+++ emacs.d/init-mode.d/60-jabber.el
@@ -0,0 +1,10 @@
+
+(setq jabber-alert-presence-hooks nil)
+(setq jabber-history-enable-rotation t)
+(setq jabber-history-enabled t)
+(setq jabber-roster-line-format " %c %-25n %u %-8s  %S")
+
+(setq jabber-account-list
+      '(("russell.sim@gmail.com"
+         (:network-server . "talk.google.com")
+         (:connection-type . ssl))))
