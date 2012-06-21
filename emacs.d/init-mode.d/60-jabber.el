a1fa34e2d4f4813d9cd406498b78f0403ada18f6
Russell Sim <russell.sim@gmail.com>
2012-06-21 21:31:58 +1000

    Disable help on roster by default

--- emacs.d/init-mode.d/60-jabber.el
+++ emacs.d/init-mode.d/60-jabber.el
@@ -4,6 +4,7 @@
 (setq jabber-history-enable-rotation t)
 (setq jabber-history-enabled t)
 (setq jabber-roster-line-format " %c %-25n %u %-8s  %S")
+(setq jabber-roster-show-bindings nil)
 
 (setq jabber-account-list
       '(("russell.sim@gmail.com"
