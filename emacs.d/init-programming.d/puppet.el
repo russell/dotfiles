2f6cf07f31d3e0c6f8db9bcd34cfb8604b40fa4d
Russell Sim <russell.sim@gmail.com>
2012-06-23 17:01:53 +1000

    Puppet region align now works with strings that contain {}

--- emacs.d/init-programming.d/puppet.el
+++ emacs.d/init-programming.d/puppet.el
@@ -1,13 +1,20 @@
 (setq puppet-mode-hook nil)
 
+(defun puppet-flash-region (start end &optional timeout)
+  "Temporarily highlight region from START to END."
+  (let ((overlay (make-overlay start end)))
+    (overlay-put overlay 'face 'secondary-selection)
+    (run-with-timer (or timeout 0.2) nil 'delete-overlay overlay)))
+
 (defun puppet-block-align ()
   "align the => characters for a block"
   (interactive)
   (save-excursion
-    (let ((apoint (search-backward "{" nil t))
-          (epoint (search-forward "}" nil t)))
+    (let ((apoint (search-backward " {" nil t))
+          (epoint (re-search-forward "}[\n \t]" nil t)))
       (when apoint
-        (align-regexp apoint epoint "\\(\\s-*\\)=>" 1 1)))))
+        (align-regexp apoint epoint "\\(\\s-*\\)=>" 1 1)
+        (puppet-flash-region apoint epoint)))))
 
 (add-hook 'puppet-mode-hook
           (lambda ()
