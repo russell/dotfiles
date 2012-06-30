97490ea395ef3a13dbe1d228bef7f024d656b548
Russell Sim <russell.sim@gmail.com>
2012-06-30 17:08:23 +1000

    Added some ido customisitations

--- emacs.d/init-mode.d/40-ido.el
+++ emacs.d/init-mode.d/40-ido.el
@@ -4,3 +4,9 @@
 (setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
 (setq ido-enable-flex-matching t) ;; enable fuzzy matching
 (setq ido-everywhere t)
+(setq ido-auto-merge-work-directories-length 0)
+(setq ido-enter-matching-directory (quote first))
+(setq ido-ignore-files (quote ("\\`CVS/" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./" "\\`\\.DS_Store" "\\`\\.localized" "\\.sparsebundle/" "\\.dmg\\'")))
+(setq ido-use-virtual-buffers t)
+
+(global-set-key (kbd "C-x B") 'ido-switch-buffer-other-window)
