9222c6c3f19a122bc3e8ae6ac74dc7eeb70b2b40
Russell Sim <russell.sim@gmail.com>
2012-07-07 22:45:14 +1000

    Changed idomenu to helm imenu

--- emacs.d/init-mode.d/40-helm.el
+++ emacs.d/init-mode.d/40-helm.el
@@ -1,2 +1,5 @@
+(require 'helm-match-plugin)
 
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
+;; Jump to a definition in the current file. (This is awesome)
+(global-set-key (kbd "C-x C-i") 'helm-imenu)
--- emacs.d/init-mode.d/50-idomenu.el
+++
@@ -1,3 +0,0 @@
-
-;; Jump to a definition in the current file. (This is awesome)
-(global-set-key (kbd "C-x C-i") 'idomenu)
