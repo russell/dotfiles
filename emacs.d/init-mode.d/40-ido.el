10f0ae1c26175087fd5f3acdbf7a8ab36443552b
Russell Sim <russell.sim@gmail.com>
2012-07-07 20:15:05 +1000

    Adde ido recent files method

--- emacs.d/init-mode.d/40-ido.el
+++ emacs.d/init-mode.d/40-ido.el
@@ -4,9 +4,35 @@
 (setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
 (setq ido-enable-flex-matching t) ;; enable fuzzy matching
 (setq ido-everywhere t)
+(setq ido-mode 'both)
 (setq ido-auto-merge-work-directories-length 0)
 (setq ido-enter-matching-directory (quote first))
 (setq ido-ignore-files (quote ("\\`CVS/" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./" "\\`\\.DS_Store" "\\`\\.localized" "\\.sparsebundle/" "\\.dmg\\'")))
 (setq ido-use-virtual-buffers t)
 
+
+(defun recentf-ido-find-file ()
+  "Find a recent file using Ido."
+  (interactive)
+  (let* ((file-assoc-list
+	  (mapcar (lambda (x)
+		    (cons (file-name-nondirectory x)
+			  x))
+		  recentf-list))
+	 (filename-list
+	  (remove-duplicates (mapcar #'car file-assoc-list)
+			     :test #'string=))
+	 (filename (ido-completing-read "Choose recent file: "
+					filename-list
+					nil
+					t)))
+    (when filename
+      (find-file (cdr (assoc filename
+			     file-assoc-list))))))
+
+
 (global-set-key (kbd "C-x B") 'ido-switch-buffer-other-window)
+(global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
+(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
+
+(set-default 'imenu-auto-rescan t)
