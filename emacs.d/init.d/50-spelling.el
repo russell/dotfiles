b4fb44647b7112d605621094dc32bdabea781c4b
Russell Sim <russell.sim@gmail.com>
2012-07-08 09:15:55 +1000

    Changed to British dictionary.

--- emacs.d/init.d/50-spelling.el
+++ emacs.d/init.d/50-spelling.el
@@ -1,5 +1,5 @@
 ;; enable aspell
 (setq ispell-program-name "aspell")
 (setq ispell-list-command "list")
-(setq ispell-dictionary "english")
+(setq ispell-dictionary "en_GB")
 (setq flyspell-issue-welcome-flag nil) ;; fix flyspell problem
