de14e0607ee6146da783934c14946dd185c7b601
Russell Sim <russell.sim@gmail.com>
2012-07-17 21:25:40 +1000

    Dirtrack and better prompt

--- emacs.d/init-mode.d/40-eshell.el
+++ emacs.d/init-mode.d/40-eshell.el
@@ -4,10 +4,16 @@
 
 (setq eshell-history-size 1000)
 (setq eshell-term-name "eterm-color")
+
 (setq eshell-prompt-function
       (lambda nil
-        (concat (abbreviate-file-name (eshell/pwd))
-                (if (= (user-uid) 0) " # " " $ "))))
+        (concat
+         (propertize (concat
+                      (or (file-remote-p default-directory 'host) "localhost")
+                      ":") 'face '(:foreground "#ff4b4b"))
+         (if (= (user-uid) 0) " # " " $ "))))
+
+(setq eshell-highlight-prompt nil)
 
 (setq eshell-visual-commands '("vi" "screen" "top" "less" "more" "lynx" "ncftp" "pine" "tin" "trn" "elm" "aptitude" "tig"))
 
@@ -17,7 +23,13 @@
                      eshell-ls eshell-pred eshell-prompt
                      eshell-rebind eshell-script eshell-smart
                      eshell-term eshell-unix eshell-xtra))
+
 (setq eshell-buffer-shorthand t)
+
+(defun add-mode-line-dirtrack ()
+  (add-to-list 'mode-line-buffer-identification
+               '(:propertize (" " default-directory " ") face dired-directory)))
+(add-hook 'eshell-mode-hook 'add-mode-line-dirtrack)
 
 (defun my-eshell (&optional buffer)
   (interactive)
