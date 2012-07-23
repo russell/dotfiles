ee871ef6ac801db118c43441676b36c1e8ffb0e8
Russell Sim <russell.sim@gmail.com>
2012-07-24 07:59:28 +1000

    Added eval-buffer key, extra project roots.

--- emacs.d/init-mode.d/40-ibuffer.el
+++ emacs.d/init-mode.d/40-ibuffer.el
@@ -26,11 +26,12 @@
 (defun ibuffer-ido-find-file ()
   "Like `ido-find-file', but default to the directory of the buffer at point."
   (interactive
-   (let ((default-directory (let ((buf (ibuffer-current-buffer)))
-			      (if (buffer-live-p buf)
-				  (with-current-buffer buf
-				    default-directory)
-				default-directory))))
+   (let ((default-directory
+           (let ((buf (ibuffer-current-buffer)))
+             (if (buffer-live-p buf)
+                 (with-current-buffer buf
+                   default-directory)
+               default-directory))))
      (ido-find-file-in-dir default-directory))))
 
 (define-key ibuffer-mode-map "\C-x\C-f" 'ibuffer-ido-find-file)
