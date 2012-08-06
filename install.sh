63902f77965c8ee7a8fab63a4f8457bddb44a72e
Russell Sim <russell.sim@gmail.com>
2012-08-06 19:48:31 +1000

    Removed printing on existing symlink.

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ for name in *; do
     if [ -e $target ]; then
 	if [ ! -L $target ]; then
 		echo "WARNING: $target exists but is not a symlink."
-	    else
-		echo "WARNING: $target exists and is a symlink."
 	fi
     else
 	if [[ $name != 'install.sh' && $name != 'README' && $name != *~  && $name != *.orig && $name != \#*\# ]]; then
