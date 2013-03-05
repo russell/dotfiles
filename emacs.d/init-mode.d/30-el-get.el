335ce079d9cf3267d57d0544fe4594ef0b27e239
Russell Sim <russell.sim@gmail.com>
2013-03-05 18:59:14 +1100

    Added emms.

--- emacs.d/init-mode.d/30-el-get.el
+++ emacs.d/init-mode.d/30-el-get.el
@@ -202,6 +202,23 @@ variable. Automatically applies expand-file-name to `path`."
                :url "git://github.com/emacs-helm/helm.git"
                :features (helm))
 
+        (:name emms
+               :description "The Emacs Multimedia System"
+               :type git
+               :url "http://git.sv.gnu.org/r/emms.git"
+               :info "doc"
+               :load-path ("./lisp")
+               :features emms-setup
+               :build `(,(format "mkdir -p %s/emms " user-emacs-directory)
+                        ,(concat "make EMACS=" el-get-emacs
+                                 " SITEFLAG=\"--no-site-file -L " el-get-dir "/emacs-w3m/ \""
+                                 " autoloads lisp docs"))
+               :depends emacs-w3m)
+
+        (:name ampc
+               :type git
+               :url "http://ch.ristopher.com/r/ampc")
+
         (:name yasnippet
                :website "http://code.google.com/p/yasnippet/"
                :description "YASnippet is a template system for Emacs."
