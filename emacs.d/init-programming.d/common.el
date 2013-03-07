37610cba80e025ef1f7d5aeff87a5b717c585000
Russell Sim <russell.sim@gmail.com>
2013-03-07 14:27:24 +1100

    Fixed some minor bugs in the programming mode specifications.

--- emacs.d/init-programming.d/common.el
+++ emacs.d/init-programming.d/common.el
@@ -1,6 +1,6 @@
 (mapc (lambda (mode)
         ;; diff hl mode
-        (add-hook mode 'diff-hl)
+        (add-hook mode 'diff-hl-mode)
 
         ;; Delete whitespace on save.
         (add-hook mode
@@ -12,7 +12,7 @@
 
         ;; flyspell
         (add-hook mode 'flyspell-prog-mode)
-        (add-hook mode 'highlight-symbol))
+        (add-hook mode 'highlight-symbol-mode))
 
-      '(python-mode slime-mode-hook geiser-mode-hook emacs-lisp-mode-hook
-                    c-mode-common-hook puppet-mode-hook sh-mode-hook))
+      '(python-mode-hook slime-mode-hook geiser-mode-hook emacs-lisp-mode-hook
+                         c-mode-common-hook puppet-mode-hook sh-mode-hook))
