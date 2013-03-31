617f91aede33da914e36a72915be67d61ac9a928
Russell Sim <russell.sim@gmail.com>
2013-03-31 15:04:11 +1100

    Moved helm configuration to el-get style section.

--- emacs.d/el-get-init/init-helm.el
+++ emacs.d/el-get-init/init-helm.el
@@ -1,1 +1,5 @@
 (global-set-key "\M-y" 'helm-show-kill-ring)
+
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+;; Jump to a definition in the current file. (This is awesome)
+(global-set-key (kbd "C-x C-i") 'helm-imenu)
--- emacs.d/init-mode.d/40-helm.el
+++
@@ -1,6 +0,0 @@
-(require 'helm-match-plugin)
-(require 'helm-files)
-
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
-;; Jump to a definition in the current file. (This is awesome)
-(global-set-key (kbd "C-x C-i") 'helm-imenu)
