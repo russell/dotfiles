125fbc058d7df4e2a10151872c86ebc96c1e3314
Russell Sim <russell.sim@gmail.com>
2013-04-02 12:38:22 +1100

    Added erc notification and other erc modifications.

--- emacs.d/init-mode.d/60-erc.el
+++ emacs.d/init-mode.d/60-erc.el
@@ -16,6 +16,7 @@
 (setq erc-server-auto-reconnect nil)
 (setq erc-server-reconnect-attempts 5)
 (setq erc-server-reconnect-timeout 5)
+(setq erc-nick-notify-icon "/usr/share/pixmaps/other/IRC.png")
 
 ;; try and prevent ERC from flooding the connection when trying to
 ;; reconnect
@@ -23,23 +24,47 @@
 
 (add-hook 'erc-join-hook 'enable-editing-modes)
 
+;;; erc-nick-notify
+(autoload 'erc-nick-notify-mode "erc-nick-notify"
+  "Minor mode that calls `erc-nick-notify-cmd' when his nick gets
+mentioned in an erc channel" t)
+(eval-after-load 'erc '(erc-nick-notify-mode t))
+
 (setq erc-autojoin-channels-alist
       '(("freenode.net" "#emacs" "#python"
          "#openstack" "#lisp" "#lispcafe"
          "#clnoobs")
         ("oftc.net" "#debian" "#debian-mentors")
-        ("austnet.org" "#nectar")))
+        ("irc.in.f2l.info" "#befit")))
+
+(defun irc-oftc ()
+  (interactive)
+  (erc-tls :server "irc.oftc.net" :port 6697
+	   :nick "arrsim" :full-name "Russell Sim"
+	   :password oftc-pass))
+
+(defun irc-freenode ()
+  (interactive)
+  (erc-tls :server "irc.freenode.net" :port 6697
+	   :nick "arrsim" :full-name "Russell Sim"
+	   :password freenode-pass))
+
+(defun irc-bitlbee ()
+  (interactive)
+  (erc :server "localhost" :port 6667
+	   :nick "arrsim" :full-name "Russell Sim"
+       :password "vi8huoKo"))
+
+(defun irc-f2l ()
+  (interactive)
+  (erc :server "irc.in.f2l.info" :port 6667
+	   :nick "arrsim" :full-name "Russell Sim"))
 
 (defun start-irc ()
   "Connect to IRC."
   (interactive)
   (setq frame-title-format '("ERC: %b"))
-  (erc-tls :server "irc.oftc.net" :port 6697
-	   :nick "arrsim" :full-name "Russell Sim"
-	   :password oftc-pass)
-  (erc-tls :server "irc.freenode.net" :port 6697
-	   :nick "arrsim" :full-name "Russell Sim"
-	   :password freenode-pass)
-  (erc-tls :server "scoutlan.qld.au.austnet.org" :port 6697
-           :nick "arrsim" :full-name "Russell Sim"
-           :password austnet-pass))
+  (irc-oftc)
+  (irc-freenode)
+  (irc-bitlbee)
+  (irc-f2l))
