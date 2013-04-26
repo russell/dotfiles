d2de81b43fe804763680ecaf22380ab3c90466dc
Russell Sim <russell.sim@gmail.com>
2013-04-26 17:17:01 +1000

    Added ical events and smartparens

---
+++ emacs.d/el-get-init/init-smartparens.el
@@ -0,0 +1,11 @@
+;;; markdown-mode
+(require 'smartparens)
+(sp-with-modes '(markdown-mode gfm-mode rst-mode)
+  (sp-local-pair "*" "*" :bind "C-*")
+  (sp-local-tag "2" "**" "**")
+  (sp-local-tag "s" "```scheme" "```")
+  (sp-local-tag "<"  "<_>" "</_>" :transform 'sp-match-sgml-tags))
+
+;;; html like modes
+(sp-with-modes '(html-mode sgml-mode)
+  (sp-local-pair "<" ">"))
