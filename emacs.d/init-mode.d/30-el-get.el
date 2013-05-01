0171315dd710e538aa581980b9a92db08f2963d0
Russell Sim <russell.sim@gmail.com>
2013-05-01 10:36:59 +1000

    Removed custom cedet recipe.

--- emacs.d/init-mode.d/30-el-get.el
+++ emacs.d/init-mode.d/30-el-get.el
@@ -68,17 +68,6 @@
                :after (progn
                         (setq autopair-blink t)))
 
-        (:name cedet
-               :website "http://cedet.sourceforge.net/"
-               :description "CEDET is a Collection of Emacs Development Environment Tools written with the end goal of creating an advanced development environment in Emacs."
-               :type git
-               :url "git://github.com/emacsmirror/cedet.git"
-               :build ("touch `find . -name Makefile`" "make")
-               :build/windows-nt ("echo #!/bin/sh > tmp.sh & echo touch `/usr/bin/find . -name Makefile` >> tmp.sh & echo make FIND=/usr/bin/find >> tmp.sh"
-                                  "sed 's/^M$//' tmp.sh  > tmp2.sh"
-                                  "sh ./tmp2.sh" "rm ./tmp.sh ./tmp2.sh")
-               )
-
         (:name django-mode
                :type git
                :url "https://github.com/myfreeweb/django-mode.git")
