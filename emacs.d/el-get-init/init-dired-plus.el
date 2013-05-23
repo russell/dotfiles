accfe9c3fc8b769dd7021c7b4bac67328713594f
Russell Sim <russell.sim@gmail.com>
2013-05-23 15:23:27 +1000

    Added simpler colours for dired-plus mode.

---
+++ emacs.d/el-get-init/init-dired-plus.el
@@ -0,0 +1,6 @@
+(custom-set-faces
+ '(diredp-exec-priv ((t nil)))
+ '(diredp-no-priv ((t nil)))
+ '(diredp-rare-priv ((t (:foreground "Green"))))
+ '(diredp-read-priv ((t nil)))
+ '(diredp-write-priv ((t nil))))
