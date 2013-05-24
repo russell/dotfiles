2143c93c64bb54b2ee287c17153ff2bbe4514d30
Russell Sim <russell.sim@gmail.com>
2013-05-24 16:51:55 +1000

    Moved magit initialisation.

--- emacs.d/el-get-init/init-magit.el
+++ emacs.d/el-get-init/init-magit.el
@@ -4,8 +4,6 @@
 (add-hook 'magit-log-edit-mode-hook 'flyspell-mode)
 (add-hook 'magit-log-edit-mode-hook 'turn-on-artbollocks-mode)
 
-(global-set-key (kbd "C-x m") 'magit-status)
-
 ;; Git-Commit-Mode
 (add-hook 'git-commit-mode-hook 'turn-on-flyspell)
 (add-hook 'git-commit-mode-hook 'auto-capitalize-mode)
