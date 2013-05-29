ee4e92153f34a5d141a7692086b1a996c9ad3207
Russell Sim <russell.sim@gmail.com>
2013-05-30 09:44:20 +1000

    Added highlight indention, erc, helm and mumamo.

--- emacs.d/arrsim-custom.el
+++ emacs.d/arrsim-custom.el
@@ -7,4 +7,22 @@
 
 (custom-theme-set-faces
  'arrsim-custom
- '(highlight-indentation-face ((t (:background "grey25")))))
+ '(highlight-indentation-face ((t (:background "grey25"))))
+ '(helm-selection ((t (:inherit 'highlight))))
+ ;; mumamo-mode
+ '(mumamo-background-chunk-major ((t (:background "#2e3436"))))
+ '(mumamo-background-chunk-submode1 ((t (:background "grey30"))))
+ '(mumamo-background-chunk-submode2 ((t (:background "grey30"))))
+ '(mumamo-background-chunk-submode3 ((t (:background "grey30"))))
+ '(mumamo-background-chunk-submode4 ((t (:background "grey30"))))
+
+ ;; artbollock-mode
+ '(font-lock-passive-voice-face ((t (:stipple nil :inverse-video nil :underline "gold" :slant italic))))
+ '(font-lock-lexical-illusions-face ((t (:underline "limegreen" :weight bold :slant italic :inverse-video nil :stipple nil))))
+ '(font-lock-weasel-words-face ((t (:weight bold :slant italic :inverse-video nil :underline "Brown" :stipple nil))))
+ '(font-lock-artbollocks-face ((t (:stipple nil :underline "Purple" :inverse-video nil :slant italic :weight bold))))
+ '(flymake-infoline ((t (:background "#00472b"))))
+ '(flymake-warnline ((t (:background "#001B63"))))
+ ;; erc
+ '(erc-prompt-face ((t (:inherit minibuffer-prompt))))
+ )
