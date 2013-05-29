dbfb7848be00dc206fa3cc204e038f46f701bfe5
Russell Sim <russell.sim@gmail.com>
2013-05-29 11:25:18 +1000

    Fix rst mode faces.

--- emacs.d/arrsim-custom.el
+++ emacs.d/arrsim-custom.el
@@ -1,6 +1,10 @@
 (deftheme arrsim-custom
   "Created 2013-05-29.")
 
+(custom-theme-set-variables
+ 'arrsim-custom
+ '(setq 'rst-level-face-base-color "grey25"))
+
 (custom-theme-set-faces
  'arrsim-custom
  '(highlight-indentation-face ((t (:background "grey25")))))
