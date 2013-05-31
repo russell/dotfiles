db9bc43b14bcdc0ef331782dc55f17d83d978f93
Russell Sim <russell.sim@gmail.com>
2013-05-31 16:00:17 +1000

    Added new autocomplete configuration.

--- emacs.d/el-get-init/init-auto-complete.el
+++ emacs.d/el-get-init/init-auto-complete.el
@@ -8,11 +8,20 @@
 
 (custom-set-variables
  '(ac-trigger-key "TAB")
+ '(ac-disable-inline t)
  '(ac-dwim t))
 
 ;; SLIME
 (add-hook 'slime-mode-hook
           '(lambda ()
              (require 'ac-slime)
-             (setq ac-sources '(ac-source-abbrev ac-source-words-in-same-mode-buffers
-                                                 ac-source-slime-fuzzy))))
+             (setq ac-sources '(ac-source-abbrev
+                                ac-source-words-in-same-mode-buffers
+                                ac-source-slime-fuzzy))))
+
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (setq ac-sources '(ac-source-yasnippet
+                                ac-source-abbrev
+                                ac-source-words-in-same-mode-buffers
+                                ac-source-jedi-direct))))
