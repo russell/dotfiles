568af1e43ab1afba9ddb88ccac10a35308f92c67
Russell Sim <russell.sim@gmail.com>
2013-06-23 15:13:17 +1000

    Updated install script to support manual path specification. Added gnus and conkeror desktop files and added gtk3 settings.

--- install.sh
+++ install.sh
@@ -1,21 +1,62 @@
 #!/bin/bash
 
+files=$(cat <<EOF
+bash_aliases
+bashrc
+bazaar
+conkerorrc
+eclrc
+emacs
+emacs.d
+gbp.conf
+gitconfig
+gitignore
+gnus
+gtkrc-2.0
+gvimrc
+hgrc
+inputrc
+keysnail.js
+mutt
+muttrc
+pbuilderrc
+profile
+sbclrc
+stumpwmrc
+vim
+vimrc
+xmodmaprc
+Xresources
+zsh
+zshrc
+EOF
+)
 
-for name in *; do
-    target="$HOME/.$name"
+function symlink {
+    name=$1
+    target=${2:-"$HOME/.$name"}
     if [ -e $target ]; then
-	if [ ! -L $target ]; then
-		echo "WARNING: $target exists but is not a symlink."
-	fi
-    else
-	if [[ $name != 'install.sh' && $name != 'README' && $name != *~  && $name != *.orig && $name != \#*\# ]]; then
-	    if [[ $name == *.local ]]; then
-		echo "added local file $target"
-		cp "$PWD/$name" "$target"
-	    else
-		echo "linked in $target"
-		ln -s "$PWD/$name" "$target"
+	    if [ ! -L $target ]; then
+		    echo "WARNING: $target exists but is not a symlink."
 	    fi
-	fi
+    else
+	    if [[ $name != *~  && $name != *.orig && $name != \#*\# ]]; then
+	        if [[ $name == *.local ]]; then
+		        echo "added local file $target"
+		        cp "$PWD/$name" "$target"
+	        else
+		        echo "linked in $target"
+		        ln -s "$PWD/$name" "$target"
+	        fi
+	    fi
     fi
+}
+
+
+for name in $files; do
+    symlink $name
 done
+
+symlink config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+symlink local/share/applications/conkeror.desktop ~/.local/share/applications/conkeror.desktop
+symlink local/share/applications/gnus.desktop ~/.local/share/applications/gnus.desktop
