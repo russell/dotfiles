403197944971c696ee48f6e13ceffffb2672e5aa
Russell Sim <russell.sim@gmail.com>
2013-06-30 11:30:04 +1000

    Override orgmode recipe.

--- emacs.d/init-mode.d/30-el-get.el
+++ emacs.d/init-mode.d/30-el-get.el
@@ -525,6 +525,24 @@
                :features ibuffer-tramp
                :url "git://github.com/svend/ibuffer-tramp.git")
 
+        (:name org-mode
+               :website "http://orgmode.org/"
+               :description "Org-mode is for keeping notes, maintaining ToDo lists, doing project planning, and authoring with a fast and effective plain-text system."
+               :type git
+               :url "git://orgmode.org/org-mode.git"
+               :info "doc"
+               :build/berkeley-unix `,(mapcar
+                                       (lambda (target)
+                                         (list "gmake" target (concat "EMACS=" (shell-quote-argument el-get-emacs))))
+                                       '("oldorg"))
+               :build `,(mapcar
+                         (lambda (target)
+                           (list "make" target (concat "EMACS=" (shell-quote-argument el-get-emacs))))
+                         '("oldorg"))
+               :load-path ("." "lisp" "contrib/lisp")
+               :autoloads nil
+               :features org-loaddefs)
+
         (:name jabber
                :type git
                :load-path ("." "compat")
