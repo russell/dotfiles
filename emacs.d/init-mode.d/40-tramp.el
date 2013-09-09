421221ab70b79d192041059f5f0814e1ed3e437a
Russell Sim <russell.sim@gmail.com>
2013-09-10 09:52:53 +1000

    Removed proxies from tramp.

--- emacs.d/init-mode.d/40-tramp.el
+++ emacs.d/init-mode.d/40-tramp.el
@@ -2,26 +2,6 @@
 (setq password-cache-expiry 1000)
 (set-default 'tramp-default-proxies-alist '())
 (add-to-list 'tramp-default-proxies-alist
-	     '(nil "\\`root\\'" "/ssh:%h:"))
-(add-to-list 'tramp-default-proxies-alist
-	     '("10\\.42\\.33\\.1" nil nil))
-(add-to-list 'tramp-default-proxies-alist
-	     '("\\.rc\\.nectar\\.org\\.au" nil "/ssh:root@ra-np.melbourne.nectar.org.au:"))
-(add-to-list 'tramp-default-proxies-alist
-	     '("115\\.146\\.83" nil nil))
-(add-to-list 'tramp-default-proxies-alist
-	     '("115\\.146\\.90" nil nil))
-(add-to-list 'tramp-default-proxies-alist
-	     '("115\\.146\\.92" nil nil))
-(add-to-list 'tramp-default-proxies-alist
-	     '("115\\.146\\.93" nil nil))
-(add-to-list 'tramp-default-proxies-alist
-	     '("115\\.146\\.94" nil nil))
-(add-to-list 'tramp-default-proxies-alist
-	     '("115\\.146\\.95" nil nil))
-(add-to-list 'tramp-default-proxies-alist
-	     '("10\\.42\\.33\\.1" nil nil))
-(add-to-list 'tramp-default-proxies-alist
              '((regexp-quote (system-name)) nil nil))
 (add-to-list 'tramp-default-proxies-alist
              '((regexp-quote "localhost") nil nil))
