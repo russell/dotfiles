9c99935142cefc023bd200f3877d3ee5f0583db7
Russell Sim <russell.sim@gmail.com>
2013-09-11 21:05:23 +1000

    Revert to using standard ansi-term mode.

--- emacs.d/init-mode.d/40-term.el
+++ emacs.d/init-mode.d/40-term.el
@@ -17,48 +17,16 @@
 (ad-activate 'term-handle-ansi-terminal-messages)
 
 
-(defun my-ansi-term (program &optional new-buffer-name)
-  (interactive (let ((default-prog (or explicit-shell-file-name
-                                       (getenv "ESHELL")
-                                       shell-file-name
-                                       (getenv "SHELL")
-                                       "/bin/sh")))
-                 (list (if (or (null default-prog)
-                               current-prefix-arg)
-                           (read-from-minibuffer "Run program: " default-prog)
-                         default-prog))))
-  (let ((project
-         (if vc-mode
-             (concat ": "
-                     (my-project-name))
-           "")))
+(add-hook 'term-exec-hook (lambda ()
+            (let* ((buff (current-buffer))
+                 (proc (get-buffer-process buff)))
+            (lexical-let ((buff buff))
+               (set-process-sentinel proc (lambda (process event)
+                            (if (string= event "finished\n")
+                                       (kill-buffer buff))))))))
 
-    ;; Pick the name of the new buffer.
-    (setq term-ansi-buffer-name
-          (if new-buffer-name
-              new-buffer-name
-            (if term-ansi-buffer-base-name
-                (if (eq term-ansi-buffer-base-name t)
-                    (file-name-nondirectory program)
-                  term-ansi-buffer-base-name)
-              (concat "ansi-term" project)))))
-
-  (setq term-ansi-buffer-name (concat "*" term-ansi-buffer-name "*"))
-  (if (get-buffer term-ansi-buffer-name)
-      (switch-to-buffer term-ansi-buffer-name)
-    (setq term-ansi-buffer-name (generate-new-buffer-name term-ansi-buffer-name))
-
-    (setq term-ansi-buffer-name (term-ansi-make-term term-ansi-buffer-name program))
-
-    (set-buffer term-ansi-buffer-name)
-    (term-mode)
-    (term-char-mode)
-    (let (term-escape-char)
-      ;; I wanna have find-file on C-x C-f -mm
-      ;; your mileage may definitely vary, maybe it's better to put this in your
-      ;; .emacs ...
-      (term-set-escape-char ?\C-x))
-
-    (switch-to-buffer term-ansi-buffer-name)))
+(defun my-ansi-term (&optional new-buffer-name)
+  (interactive)
+  (ansi-term "/usr/bin/zsh" new-buffer-name))
 
 (global-set-key "\C-cc" 'my-ansi-term)
