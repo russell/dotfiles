2bccd8a36445ecad262ed28613c07b5b746414eb
Russell Sim <russell.sim@gmail.com>
2013-11-19 21:48:33 +1100

    New line and indent in js2-mode

--- emacs.d/init-programming.d/javascript.el
+++ emacs.d/init-programming.d/javascript.el
@@ -5,3 +5,7 @@
 (custom-set-variables
  '(js-indent-level 2)
  '(js2-basic-offset 2))
+
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (local-set-key (kbd "RET") 'newline-and-indent)))
