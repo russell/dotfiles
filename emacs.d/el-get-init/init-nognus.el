209f5c8fc567582fb6fa4a6ef199b44c41376f66
Russell Sim <russell.sim@gmail.com>
2013-12-11 08:13:10 +1100

    Wrap in dir with trailing slash

--- emacs.d/el-get-init/init-nognus.el
+++ emacs.d/el-get-init/init-nognus.el
@@ -1,7 +1,7 @@
 
 (defadvice gnus (around gnus-around)
   "Force start in home directory."
-  (let ((default-directory (expand-file-name "~")))
+  (let ((default-directory (expand-file-name "~/")))
     ad-do-it))
 
 (ad-activate 'gnus)
