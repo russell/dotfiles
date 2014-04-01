ee6e7033f16a95bea508815e910d409cb9585a8e
Russell Sim <russell.sim@gmail.com>
2014-04-02 09:14:59 +1100

    Fixed up puppet hooks

--- emacs.d/init-programming.d/common.el
+++ emacs.d/init-programming.d/common.el
@@ -1,3 +1,7 @@
+
+;;; Code:
+(require 'cl-lib)
+
 (defun toggle-highlight-symbol ()
   (unless (eq major-mode 'slime-xref-mode)
     (highlight-symbol-mode)))
@@ -7,9 +11,8 @@
                     emacs-lisp-mode-hook))
       (c-like-modes '(python-mode-hook
                       c-mode-common-hook
-                      puppet-mode-hook
                       sh-mode-hook
-                      puppet-hook-mode
+                      puppet-mode-hook
                       makefile-mode-hook
                       js2-mode-hook
                       js-mode-hook)))
@@ -27,10 +30,10 @@
           (add-hook mode 'flyspell-prog-mode)
           (add-hook mode 'toggle-highlight-symbol))
 
-        (concatenate 'list lisp-modes c-like-modes))
+        (cl-concatenate 'list lisp-modes c-like-modes))
   (mapc (lambda (mode)
           (add-hook mode '(lambda ()
                             (smartparens-mode)
                             (sp-use-paredit-bindings))))
 
-        (concatenate 'list c-like-modes '(eshell-mode))))
+        (cl-concatenate 'list c-like-modes '(eshell-mode))))
