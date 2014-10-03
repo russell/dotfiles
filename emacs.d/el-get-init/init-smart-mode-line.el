dcf3d0e957c009cc3e2ac1bb8acfde7bd03bb046
Russell Sim <russell.sim@gmail.com>
2014-10-03 21:56:51 +1000

    Moved out rich-minority config out

--- emacs.d/el-get-init/init-smart-mode-line.el
+++ emacs.d/el-get-init/init-smart-mode-line.el
@@ -2,16 +2,6 @@
 
 (sml/setup)
 
-(add-to-list 'rm-excluded-modes " yas")
-(add-to-list 'rm-excluded-modes " hl-s")
-(add-to-list 'rm-excluded-modes " ElDoc")
-(add-to-list 'rm-excluded-modes " Paredit")
-(add-to-list 'rm-excluded-modes " SliNav")
-(add-to-list 'rm-excluded-modes " SliExp")
-(add-to-list 'rm-excluded-modes " SP")
-(add-to-list 'rm-excluded-modes " ||")
-(add-to-list 'rm-excluded-modes "/s")
-
 (add-to-list 'sml/replacer-regexp-list '("^~/projects/" ":projects:") t)
 
 (sml/apply-theme 'dark)
