100994fb000e02086689ea84ebbda5f534d093e8
Russell Sim <russell.sim@gmail.com>
2015-01-05 14:24:00 +1100

    Always use js2-mode

--- emacs.d/init-programming.d/javascript.el
+++ emacs.d/init-programming.d/javascript.el
@@ -1,10 +1,15 @@
+;;; Code:
+
+(require 'js)
+(require 'js2-mode)
+
+(defalias 'js-mode 'js2-mode)
 
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-(custom-set-variables
- '(js-indent-level 2)
- '(js2-basic-offset 2))
+(setq js-indent-level 2)
+(setq js2-basic-offset 2)
 
 (add-hook 'js2-mode-hook
           (lambda ()
