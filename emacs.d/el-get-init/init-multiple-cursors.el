cf551611c8d403743ca8aeeb7f757d9ae9362e5a
Russell Sim <russell.sim@gmail.com>
2015-02-02 18:40:04 +1100

    [Emacs] start to record a MC functions
    Start to record a list of the useful Multiple Cursors functions.

--- emacs.d/el-get-init/init-multiple-cursors.el
+++ emacs.d/el-get-init/init-multiple-cursors.el
@@ -1,3 +1,13 @@
+
+;;; Code:
+
 (require 'multiple-cursors-core)
 
-(add-to-list 'mc--default-cmds-to-run-for-all 'python-indent-dedent-line-backspace)
+(add-to-list 'mc--default-cmds-to-run-for-all
+             'hungry-delete-backward)
+
+(add-to-list 'mc--default-cmds-to-run-for-all
+             'python-indent-dedent-line-backspace)
+
+(provide 'init-multiple-cursors)
+;;; init-multiple-cursors.el
