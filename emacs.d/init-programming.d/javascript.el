0862573e40f4262a625faf4fa6ddd6c1265bff9f
Russell Sim <russell.sim@gmail.com>
2015-07-21 21:05:10 +1000

    [Emacs] Fix indenting and newline behaviour for JS

--- emacs.d/init-programming.d/javascript.el
+++ emacs.d/init-programming.d/javascript.el
@@ -9,8 +9,4 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (setq js-indent-level 2)
-(setq js2-basic-offset 2)
-
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (local-set-key (kbd "RET") 'newline-and-indent)))
+(setq-default js2-basic-offset 2)
