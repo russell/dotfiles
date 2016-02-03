bcc5557af8ef78c5a8dd43e30463ee6e1be24d5e
Russell Sim <russell.sim@gmail.com>
2016-02-04 07:43:15 +1100

    [Emacs] Fixed flycheck loadpath for el-get

--- emacs.d/el-get-init/init-flycheck.el
+++ emacs.d/el-get-init/init-flycheck.el
@@ -1,5 +1,16 @@
 
-(custom-set-variables
- '(flycheck-highlighting-mode 'lines)
- '(flycheck-idle-change-delay 2)
- '(flycheck-indication-mode 'right-fringe))
+(require 'use-package)
+
+;;; Code:
+
+(use-package flycheck
+  :commands flycheck-mode
+  :config
+  (setq-default flycheck-emacs-lisp-load-path 'load-path)
+  (setq flycheck-highlighting-mode 'lines)
+  (setq flycheck-idle-change-delay 2)
+  (setq flycheck-indication-mode 'right-fringe))
+
+(provide 'init-flycheck)
+
+;;; init-flycheck.el ends here
