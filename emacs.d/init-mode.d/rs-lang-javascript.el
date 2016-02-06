e0813adf003c3b513ae06028d43561d8cc6de5bd
Russell Sim <russell.sim@gmail.com>
2016-02-07 05:58:05 +1100

    [Emacs] Enable smartparens for js2-mode

--- emacs.d/init-mode.d/rs-lang-javascript.el
+++ emacs.d/init-mode.d/rs-lang-javascript.el
@@ -12,7 +12,8 @@
   (autoload 'js2-mode "js2-mode" nil t)
   (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-  (setq-default js2-basic-offset 2))
+  (setq-default js2-basic-offset 2)
+  (add-hook 'js2-mode-hook 'smartparens-mode))
 
 (use-package js
   :config
