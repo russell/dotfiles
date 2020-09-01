3b8743dd1658afa915ee9e913dc236d603e5f766
Russell Sim <russell.sim@gmail.com>
2020-09-01 22:04:32 +0200

    [emacs] cleanup package files

--- emacs/.spacemacs.d/rs-writing/packages.el
+++ emacs/.spacemacs.d/rs-writing/packages.el
@@ -9,56 +9,12 @@
 ;;
 ;;; License: GPLv3
 
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `rs-writing-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `rs-writing/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `rs-writing/pre-init-PACKAGE' and/or
-;;   `rs-writing/post-init-PACKAGE' to customize the package as it is loaded.
-
 ;;; Code:
 
 (defconst rs-writing-packages
   '(writegood-mode
     markdown-mode
-    (org-mode :location built-in))
-  "The list of Lisp packages required by the rs-writing layer.
-
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+    (org-mode :location built-in)))
 
 (defun rs-writing/post-init-flycheck ()
   (flycheck-define-checker proselint
