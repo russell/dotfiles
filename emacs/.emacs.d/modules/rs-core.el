1108852ebc6401c42f7ab144e6625795d0114d6b
Russell Sim <russell.sim@gmail.com>
2020-09-11 18:41:20 +0200

    [emacs] add winum mode

--- emacs/.emacs.d/modules/rs-core.el
+++ emacs/.emacs.d/modules/rs-core.el
@@ -24,7 +24,7 @@
 
 ;;; Code:
 
-(prelude-require-packages '(envrc))
+(prelude-require-packages '(envrc winum))
 
 (setq confirm-kill-emacs 'yes-or-no-p)
 (put 'set-goal-column 'disabled nil)
@@ -53,6 +53,23 @@
 (use-package envrc
   :init
   (envrc-global-mode))
+
+
+(use-package winum
+  :init
+  (winum-mode)
+  :bind
+  (("M-0" . 'winum-select-window-0-or-10)
+   ("M-1" . 'winum-select-window-1)
+   ("M-2" . 'winum-select-window-2)
+   ("M-3" . 'winum-select-window-3)
+   ("M-4" . 'winum-select-window-4)
+   ("M-5" . 'winum-select-window-5)
+   ("M-6" . 'winum-select-window-6)
+   ("M-7" . 'winum-select-window-7)
+   ("M-8" . 'winum-select-window-8)
+   ("M-9" . 'winum-select-window-9)))
+
 
 (provide 'rs-core)
 ;;; rs-core.el ends here
