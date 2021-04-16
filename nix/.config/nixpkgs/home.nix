24925c07aff486084bdf4fb7a06f5eb6a41fd420
Russell Sim <russell.sim@gmail.com>
2021-04-16 15:17:46 +0200

    [nix] add direnv

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -4,6 +4,9 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  programs.direnv.enable = true;
+  programs.direnv.enableNixDirenvIntegration = true;
+
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = (builtins.getEnv "USER");
