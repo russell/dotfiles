2d2cc21b4a3d35cf140a60f459ad9552636d1bb4
Russell Sim <russell.sim@gmail.com>
2021-06-09 21:22:52 +0200

    [x11] move picom to systemd

--- install.sh
+++ install.sh
@@ -38,10 +38,15 @@ EOF
 # Remove all the .DS_Store files
 find . -name .DS_Store -exec rm {} \;
 
+mkdir -p ~/.config/systemd/user
+
 for name in $packages; do
     echo "Stowing package $name"
     stow --dotfiles -t "$HOME" -v $name
 done
+
+systemctl --user enable xfce4-power-manager
+systemctl --user enable picom
 
 if [ ! -e ~/.emacs-prelude.d ]; then
     git clone git://github.com/bbatsov/prelude.git ~/.emacs-prelude.d
