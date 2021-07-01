2d7d07bd8251681000845c64e9a9942edca210f2
Russell Sim <russell.sim@gmail.com>
2021-07-01 18:56:53 +0200

    [emacs] update theme to remove borders

--- emacs/.emacs.d/rs-dark-theme.el
+++ emacs/.emacs.d/rs-dark-theme.el
@@ -1,10 +1,12 @@
 (deftheme rs-dark
-  "Created 2021-06-30.")
+  "Created 2021-07-01.")
 
 (custom-theme-set-faces
  'rs-dark
  '(window-divider ((t (:foreground "#000000"))))
  '(window-divider-last-pixel ((t (:foreground "#000000"))))
- '(window-divider-first-pixel ((t (:foreground "#000000")))))
+ '(window-divider-first-pixel ((t (:foreground "#000000"))))
+ '(mode-line ((t (:box nil))))
+ '(mode-line-inactive ((t (:box nil)))))
 
 (provide-theme 'rs-dark)
--- emacs/.emacs.d/rs-light-theme.el
+++ emacs/.emacs.d/rs-light-theme.el
@@ -5,6 +5,8 @@
  'rs-light
  '(window-divider ((t (:foreground "#fff"))))
  '(window-divider-last-pixel ((t (:foreground "#fff"))))
- '(window-divider-first-pixel ((t (:foreground "#fff")))))
+ '(window-divider-first-pixel ((t (:foreground "#fff"))))
+ '(mode-line ((t (:box nil))))
+ '(mode-line-inactive ((t (:box nil)))))
 
 (provide-theme 'rs-light)
